347fe8c4e328c128fe0db19212e08ed50effe93b
JoaoCLAragao <71600841@aluno.faculdadecotemig.br>
2021-03-20 22:11:56 -0300

    SOCORRO

--- app/src/main/java/br/com/cotemig/calculoipva/MainActivity.kt
+++ app/src/main/java/br/com/cotemig/calculoipva/MainActivity.kt
@@ -1,11 +1,54 @@
 package br.com.cotemig.calculoipva
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        btn_calularIPVA.setOnClickListener {
+            calularIPVA()
+        }
+
+
+
+    }
+
+    fun calularIPVA(){
+        var valorVeiculo = e_txt_valor.toString().toDouble()
+        var tipoVeiculo = e_txt_tipo.toString().toInt()
+        var ipva : Double
+        var aliquota : Double
+
+        if (tipoVeiculo == 1){
+            aliquota = 0.015
+        }
+        else if (tipoVeiculo == 2){
+            aliquota = 0.02
+        }
+        else if (tipoVeiculo == 3){
+            aliquota = 0.02
+        }
+        else if (tipoVeiculo == 4){
+            aliquota = 0.02
+        }
+        else if (tipoVeiculo == 5){
+            aliquota = 0.03
+        }
+        else if (tipoVeiculo == 6){
+            aliquota = 0.04
+        }
+        ipva = valorVeiculo * aliquota
+
+        var intent = Intent(this, ContactsContract.ProfileActivity::class.java)
+        intent.putExtra("ipva", VALORAQUIDISGRAÇA)
+
     }
 }
---
+++ app/src/main/java/br/com/cotemig/calculoipva/ResultadoIPVA.kt
@@ -0,0 +1,11 @@
+package br.com.cotemig.calculoipva
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoIPVA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado_i_p_v_a)
+    }
+}
